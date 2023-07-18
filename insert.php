c1dba22b15a8983bafff2e4a1a7bce28a05fcec2
Dannymobile44 <139699825+Dannymobile44@users.noreply.github.com>
2023-07-18 19:43:55 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = ' ';
     $db = 'csc309';
 
     // Open a new connection
@@ -14,8 +14,8 @@
     }
 
     // Create a Table
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('Nwafor', 'Benjamin', 'benjaminn@gmail.com')";
+    $sql = "INSERT INTO users (firstname, lastname, email, gender, date_of_birth) 
+    VALUES ('Okoro', 'Daniel', 'dannymobile405@gmail.com', 'M', '1998/05/06')";
 
     $result = $con->query($sql);
 
